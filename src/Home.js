3c631c9199d4466c97da2d2a9a882922b83339b9
Gus Bikos <gusbikos@gmail.com>
2021-09-22 23:37:55 -0400

    Navbar Styling

---
+++ src/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Home.css'
+
+function Home() {
+    return (
+        <div className="home">
+            <div className="home_container">
+                <img className="home_image"
+                    src="https://m.media-amazon.com/images/I/718rhsspi4L._SX3000_.jpg" 
+                    alt=""
+                />
+            </div>
+        </div>
+    )
+}
+
+export default Home
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// connecting firebase backend to my react project
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAcIbqP22DDJLiYQ7aMVEgXmxWZRUEKxvs",
+    authDomain: "clone-a8e93.firebaseapp.com",
+    projectId: "clone-a8e93",
+    storageBucket: "clone-a8e93.appspot.com",
+    messagingSenderId: "808592992669",
+    appId: "1:808592992669:web:6bf32eed24e067e2346774",
+    measurementId: "G-LN3SM0H6GM"
+}
