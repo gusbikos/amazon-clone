98bdb5e534f6986be304d7ef38fde2895da740dc
Gus Bikos <gusbikos@gmail.com>
2021-10-04 15:01:44 -0400

    login page styling done doing auth

--- src/App.js
+++ src/App.js
@@ -2,19 +2,23 @@ import './App.css'
 import Header from './Header.js'
 import Home from './Home.js'
 import Checkout from './Checkout.js'
+import Login from './Login.js'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 const App = () => {
   return (
     <Router>
       <div classname="app">
-        <Header />  
         <Switch >
+          <Route path="/login">
+            <Login />
+          </Route >
           <Route path="/checkout">
+            <Header />  
             <Checkout />
           </Route >
           <Route path="/">
-            {/* <Header /> */}
+            <Header />
             <Home />
           </Route >
         </Switch >
--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,19 @@
 // connecting firebase backend to my react project
+import firebase from "firebase"
 
-const firebaseConfig = {
-    apiKey: "AIzaSyAcIbqP22DDJLiYQ7aMVEgXmxWZRUEKxvs",
-    authDomain: "clone-a8e93.firebaseapp.com",
-    projectId: "clone-a8e93",
-    storageBucket: "clone-a8e93.appspot.com",
-    messagingSenderId: "808592992669",
-    appId: "1:808592992669:web:6bf32eed24e067e2346774",
-    measurementId: "G-LN3SM0H6GM"
-}
+    const firebaseConfig = {
+        apiKey: "AIzaSyAcIbqP22DDJLiYQ7aMVEgXmxWZRUEKxvs",
+        authDomain: "clone-a8e93.firebaseapp.com",
+        projectId: "clone-a8e93",
+        storageBucket: "clone-a8e93.appspot.com",
+        messagingSenderId: "808592992669",
+        appId: "1:808592992669:web:6bf32eed24e067e2346774",
+        measurementId: "G-LN3SM0H6GM",
+    }
+
+    const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+    const db = firebaseApp.firestore()
+    const auth = firebase.auth()
+
+export { db, auth } 
